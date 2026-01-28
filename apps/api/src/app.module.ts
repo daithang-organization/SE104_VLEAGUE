41fe0faf746f70e373fe77bfb6ca4435f1ca1825
Dai Thang <23521422@gm.uit.edu.vn>
2026-01-28 20:01:27 +0700

    feat: add @nestjs/config dependency and configure global settings in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { RegistrationModule } from './registration/registration.module';
 
 @Module({
-  imports: [PrismaModule, RegistrationModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    RegistrationModule,
+  ],
 })
 export class AppModule {}
